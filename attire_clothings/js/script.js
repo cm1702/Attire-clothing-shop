d61e36d6fb4f3db43ba8e4132207e00fb61bd19e
cm1702 <144173161+cm1702@users.noreply.github.com>
2023-09-06 03:21:46 +0530

    Add files via upload

---
+++ attire_clothings/js/script.js
@@ -0,0 +1,39 @@
+alert('Welcome to the ATTIRE STORE');
+var $grid = $('.collection-list').isotope({
+});
+
+// filter items on button click
+
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  resetFilterBtns();
+  $(this).addClass('active-filter-btn');
+  $grid.isotope({ filter: filterValue });
+});
+
+
+var filterBtns = $('.filter-button-group').find('button');
+function resetFilterBtns(){
+  filterBtns.each(function(){
+    $(this).removeClass('active-filter-btn');
+  });
+}
+
+let mybutton = document.getElementById("myBtn");
+
+// when the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+//scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
